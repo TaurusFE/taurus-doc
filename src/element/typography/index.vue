c6e02f7fd7b18ac515d0d135e6c89d112e37163b
leopeng0805 <86646352@qq.com>
2016-12-20 21:53:05 +0800

    add typography

---
+++ src/element/typography/index.vue
@@ -0,0 +1,492 @@
+<template>
+  <div>
+    <!-- Page header -->
+    <div class="block__header leader--large trailer--large">
+      <h2 class="title--medium">Typography and lists</h2>
+    </div>
+    <!-- SG Components -->
+    <div id="font-sizes" class="doc-compontent-box">
+      <div class="sg-content">
+        <div class="sg-component__header">
+          <h2 class="title--medium-small trailer--small">Font sizes</h2>
+        </div>
+        <div class="sg-component__status trailer">
+        </div>
+        <div class="sg-content__markup">
+          <!-- atoms:Font sizes-start-->
+          <table class="table table--all-lines">
+            <thead>
+            <tr>
+              <th><strong>Class</strong></th>
+              <th><strong>Description</strong></th>
+              <th><strong>Example</strong></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>.text-size--192</td>
+              <td>192px</td>
+              <td><div class="text-size--192">Lorem</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--174</td>
+              <td>174px</td>
+              <td><div class="text-size--174">Lorem</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--114</td>
+              <td>114px</td>
+              <td><div class="text-size--114">Lorem ipsum</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--84</td>
+              <td>84px</td>
+              <td><div class="text-size--84">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--72</td>
+              <td>72px</td>
+              <td><div class="text-size--72">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--64</td>
+              <td>64px</td>
+              <td><div class="text-size--64">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--54</td>
+              <td>54px</td>
+              <td><div class="text-size--54">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--52</td>
+              <td>52px</td>
+              <td><div class="text-size--52">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--50</td>
+              <td>50px</td>
+              <td><div class="text-size--50">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--44</td>
+              <td>44px</td>
+              <td><div class="text-size--44">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--40</td>
+              <td>40px</td>
+              <td><div class="text-size--40">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--36</td>
+              <td>36px</td>
+              <td><div class="text-size--36">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--30</td>
+              <td>30px</td>
+              <td><div class="text-size--30">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--28</td>
+              <td>28px</td>
+              <td><div class="text-size--28">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--24</td>
+              <td>24px</td>
+              <td><div class="text-size--24">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--21</td>
+              <td>21px</td>
+              <td><div class="text-size--21">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--19</td>
+              <td>19px</td>
+              <td><div class="text-size--19">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--16</td>
+              <td>16px</td>
+              <td><div class="text-size--16">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--13</td>
+              <td>13px</td>
+              <td><div class="text-size--13">Lorem ipsum dolor</div></td>
+            </tr>
+            <tr>
+              <td>.text-size--11</td>
+              <td>11px</td>
+              <td><div class="text-size--11">Lorem ipsum dolor</div></td>
+            </tr>
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+    <!-- SG Components -->
+    <div id="font-weight" class="doc-compontent-box">
+      <div class="sg-content">
+        <div class="sg-component__header">
+          <h2 class="title--medium-small trailer--small">Font weight</h2>
+        </div>
+        <div class="sg-component__status trailer">
+        </div>
+        <div class="sg-content__markup">
+          <!-- atoms:Font weight-start-->
+          <table class="table table--all-lines">
+            <thead>
+            <tr>
+              <th><strong>Class</strong></th>
+              <th><strong>Description</strong></th>
+              <th><strong>Example</strong></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>.text-regular</td>
+              <td>regular</td>
+              <td class="text-regular">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.text-ligh</td>
+              <td>light</td>
+              <td class="text-light">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.text-bold</td>
+              <td>bold</td>
+              <td class="text-bold">Lorem ipsum dolor</td>
+            </tr>
+            </tbody>
+          </table>
+          <!-- atoms:Font weight-end-->
+        </div>
+      </div>
+    </div>
+
+    <!-- SG Components -->
+    <!-- SG Components -->
+    <div id="text-truncate" class="doc-compontent-box">
+      <div class="sg-content">
+        <div class="sg-component__header">
+          <h2 class="title--medium-small trailer--small">Text truncate</h2>
+        </div>
+        <div class="sg-component__status trailer">
+        </div>
+        <div class="sg-content__markup">
+          <!-- atoms:Text truncate-start-->
+          <table class="table table--all-lines">
+            <thead>
+            <tr>
+              <th><strong>Class</strong></th>
+              <th><strong>Description</strong></th>
+              <th><strong>Example</strong></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>.text-truncate</td>
+              <td>Text overflow omission</td>
+              <td><div class="text-truncate" style="width:100px">Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor</div></td>
+            </tr>
+            </tbody>
+          </table>
+          <!-- atoms:Text truncate-end-->
+        </div>
+      </div>
+    </div>
+    <!-- SG Components -->
+    <div id="color" class="doc-compontent-box">
+      <div class="sg-content">
+        <div class="sg-component__header">
+          <h2 class="title--medium-small trailer--small">Color</h2>
+        </div>
+        <div class="sg-component__status trailer">
+        </div>
+        <div class="sg-content__markup">
+          <!-- atoms:Text color-start-->
+          <table class="table table--all-lines">
+            <thead>
+            <tr>
+              <th><strong>Class</strong></th>
+              <th><strong>Description</strong></th>
+              <th><strong>Example</strong></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>.color-white</td>
+              <td>#fff</td>
+              <td class="box--blue color-white">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-blue</td>
+              <td>#038cd6</td>
+              <td class="color-blue">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-blue-light</td>
+              <td>#80c4e8</td>
+              <td class="color-blue-light">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-yellow</td>
+              <td>#ffb900</td>
+              <td class="color-yellow">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-gray</td>
+              <td>#666</td>
+              <td class="color-gray">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-gray-light</td>
+              <td>#d1d1d1</td>
+              <td class="color-gray-light">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-steel-grey</td>
+              <td>#b8b8b8</td>
+              <td class="color-steel-grey">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-smoke-grey</td>
+              <td>#e8e8e8</td>
+              <td class="color-smoke-grey">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-black-light</td>
+              <td>#474747</td>
+              <td class="color-black-light">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-green</td>
+              <td>#43c682</td>
+              <td class="color-green">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+              <td>.color-red</td>
+              <td>#db5451</td>
+              <td class="color-red">Lorem ipsum dolor</td>
+            </tr>
+            </tbody>
+          </table>
+          <!-- atoms:Text color-end-->
+        </div>
+      </div>
+    </div>
+    <!-- SG Components -->
+    <div id="link" class="doc-compontent-box">
+      <div class="sg-content">
+        <div class="sg-component__header">
+          <h2 class="title--medium-small trailer--small">Link</h2>
+        </div>
+        <div class="sg-component__status trailer">
+        </div>
+        <div class="sg-content__markup">
+          <!-- atoms:Link-start-->
+          <table class="table table--all-lines">
+            <thead>
+            <tr>
+              <th><strong>Class</strong></th>
+              <th><strong>Description</strong></th>
+              <th><strong>Example</strong></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>.link--default</td>
+              <td>Inherit color from parent block</td>
+              <td><a class="link--default">Lorem ipsum dolor</a></td>
+            </tr>
+            <tr>
+              <td>.link--underlined</td>
+              <td>Adds underline to link</td>
+              <td><a class="link--underlined">Lorem ipsum dolor</a></td>
+            </tr>
+            <tr>
+              <td>.link--non-underlined</td>
+              <td>Removes underline from link on hover</td>
+              <td><a class="link--non-underlined">Lorem ipsum dolor</a></td>
+            </tr>
+            <tr>
+              <td>.link--gray</td>
+              <td>Changes link color to $color-stone-grey(#666)</td>
+              <td><a class="link--gray">Lorem ipsum dolor</a></td>
+            </tr>
+            <tr>
+              <td>.link--white</td>
+              <td>Changes link color to $color-white(#fff), (uses inside section--negative)</td>
+              <td class="section--negative box--blue"><a class="link--white">Lorem ipsum dolor</a></td>
+            </tr>
+            <tr>
+              <td>.link--bullet</td>
+              <td>Adds bullet/arrow to link</td>
+              <td><a class="link--bullet">Lorem ipsum dolor</a></td>
+            </tr>
+            </tbody>
+          </table>
+          <!-- atoms:Link-end-->
+        </div>
+      </div>
+    </div>
+
+    <!-- SG Components -->
+    <!-- SG Components -->
+    <div id="text-align" class="doc-compontent-box">
+      <div class="sg-content">
+        <div class="sg-component__header">
+          <h2 class="title--medium-small trailer--small">Text align</h2>
+        </div>
+        <div class="sg-component__status trailer">
+        </div>
+        <div class="sg-content__markup">
+          <!-- atoms:Text align-start-->
+          <div class="sg-component__label ">Horizontal align</div>
+          <table class="table table--all-lines">
+            <thead>
+            <tr>
+              <th><strong>Class</strong></th>
+              <th><strong>Description</strong></th>
+              <th><strong>Example</strong></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>.text--left</td>
+              <td>Text level in the left</td>
+              <td><div class="text--left">left</div></td>
+            </tr>
+            <tr>
+              <td>.text--center</td>
+              <td>Text level in the center</td>
+              <td><div class="text--center">center</div></td>
+            </tr>
+            <tr>
+              <td>.text--right</td>
+              <td>Text level in the right</td>
+              <td><div class="text--right">center</div></td>
+            </tr>
+            </tbody>
+          </table>
+          <div class="sg-component__label ">Vertical align(not available at present)</div>
+          <table class="table table--all-lines">
+            <thead>
+            <tr>
+              <th><strong>Class</strong></th>
+              <th><strong>Description</strong></th>
+              <th><strong>Example</strong></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>.align--top</td>
+              <td>Vertical text top</td>
+              <td><div class="align--top" style="height: 100px">top</div></td>
+            </tr>
+            <tr>
+              <td>.align--middle</td>
+              <td>Vertical text Center</td>
+              <td><div class="align--middle" style="height: 100px">middle</div></td>
+            </tr>
+            <tr>
+              <td>.align--bottom</td>
+              <td>Vertical text bottom</td>
+              <td><div class="align--bottom" style="height: 100px">bottom</div></td>
+            </tr>
+            </tbody>
+          </table>
+          <!-- atoms:Text align-end-->
+        </div>
+      </div>
+    </div>
+
+    <div id="inline-elements" class="doc-compontent-box">
+      <div class="sg-component">
+        <div class="sg-component__header">
+          <h2 class="title--medium-small trailer--small">Inline elements</h2>
+        </div>
+        <div class="sg-component__status">
+        </div>
+        <div class="sg-component__markup">
+          <div class="demo_content">
+            <div class="text--content">
+              <div class="sg-component__label">Link</div>
+              <p>Lorem ipsum <a href="">dolor sit amet</a>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Active link</div>
+              <p>Lorem ipsum <a class="active" href="">dolor sit amet</a>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Bold text</div>
+              <p>Lorem ipsum
+                <strong>dolor sit amet</strong>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Emphasis text</div>
+              <p>Lorem ipsum
+                <em>dolor sit amet</em>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Italic text</div>
+              <p>Lorem ipsum
+                <i>dolor sit amet</i>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Underline text</div>
+              <p>Lorem ipsum
+                <u>dolor sit amet</u>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Deleted text</div>
+              <p>Lorem ipsum
+                <del>dolor sit amet</del>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Inserted text</div>
+              <p>Lorem ipsum
+                <ins>dolor sit amet</ins>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Strikethrough text</div>
+              <p>Lorem ipsum
+                <s>dolor sit amet</s>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Super text</div>
+              <p>Lorem ipsum
+                <sup>dolor sit amet</sup>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Sub text</div>
+              <p>Lorem ipsum
+                <sub>dolor sit amet</sub>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Small text</div>
+              <small>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </small>
+              <div class="sg-component__label">HTML markup</div>
+              <p>Lorem ipsum
+                <abbr title="HyperText Markup Language">dolor sit amet</abbr>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Command</div>
+              <p>Lorem ipsum
+                <kbd>dolor sit amet</kbd>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Short inline quotation</div>
+              <p>Lorem ipsum
+                <q cite="https://developer.mozilla.org/en-US/docs/HTML/Element/q">dolor sit amet</q>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Block quote</div>
+              <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lorem urna, consectetur sodales mauris eu, eleifend dictum massa. Nam cursus nulla eget justo aliquet fringilla.</blockquote>
+              <div class="sg-component__label">Citation text</div>
+              <p>Lorem ipsum
+                <cite>dolor sit amet</cite>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Definition text</div>
+              <p>Lorem ipsum
+                <dfn>dolor sit amet</dfn>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Highlighted text</div>
+              <p>Lorem ipsum
+                <mark>dolor sit amet</mark>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Code</div>
+              <p>Lorem ipsum <code>dolor sit amet</code>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Sample output</div>
+              <p>Lorem ipsum
+                <samp>dolor sit amet</samp>, consectetur adipiscing elit.</p>
+              <div class="sg-component__label">Variable element</div>
+              <p>Lorem ipsum
+                <var>dolor sit amet</var>, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+   export default {}
+</script>
