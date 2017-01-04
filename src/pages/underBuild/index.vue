432d33153a1ca01da5c29990fc6c34bbc442b132
harwinvoid <harwinyang@gmail.com>
2017-01-04 18:17:57 +0800

    增加状态页面

---
+++ src/pages/underBuild/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="errpage">
+    <div class="desc">
+      <div class="msg">
+        <div><h1 class="text-ligh">Oops!</h1></div>
+        <div class="note text-ligh">Comming soon</div>
+        <p class="note-desc">
+          We are trying to build to in the page So stay tuned...
+        </p>
+      </div>
+    </div>
+    <div class="errbox">
+      <img src="/static/img/status/503.png">
+    </div>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+export default {
+}
+</script>
